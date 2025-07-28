80c3dd0f1efe3e3f5150212bdd6dbe06afe95f8b
aleex1848 <halycon01@gmail.com>
2025-07-28 20:06:49 +0200

    fix

--- database/migrations/2025_01_22_125932_add_nutzungsart_id_to_app_raumverwaltung_raums_table.php
+++ database/migrations/2025_01_22_125932_add_nutzungsart_id_to_app_raumverwaltung_raums_table.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Apps\Raumverwaltung\Nutzungsart;
+use Hwkdo\IntranetAppRaumverwaltung\Models\Nutzungsart;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
